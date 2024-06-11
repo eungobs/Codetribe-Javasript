ae043daa7c309b7a69d99960944574ab888c90e2
eungobs <eungobs@gmail.com>
2024-06-11 23:59:49 +0200

    Create booleanOperators.js

---
+++ booleanOperators.js
@@ -0,0 +1,31 @@
+// Boolean and Operators
+let x = 8;
+let y = 12;
+
+console.log(x > y);
+console.log(x <= y);
+console.log(x === y);
+console.log(x !== y);
+
+let a = true;
+let b = false;
+
+console.log(a && b);
+console.log(a || b);
+console.log(!a);
+
+let p = 10;
+p += 5;
+console.log(p);
+
+p -= 3;
+console.log(p);
+
+p *= 2;
+console.log(p);
+
+p /= 4;
+console.log(p);
+
+p %= 3;
+console.log(p);
