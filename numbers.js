b95c4e7d057c8789f2750bfe2182e88a3201a73c
eungobs <eungobs@gmail.com>
2024-06-11 23:58:25 +0200

    Create numbers.js

---
+++ numbers.js
@@ -0,0 +1,10 @@
+// Numbers
+let number1 = 10;
+let number2 = 5.5;
+
+console.log(number1 + number2);
+console.log(number1 - number2);
+console.log(number1 * number2);
+console.log(number1 / number2);
+console.log(number1 % number2);
+console.log(number1 ** number2);
