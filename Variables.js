06c659f2866e8b7e11474957372f9ab0ecf87a17
eungobs <eungobs@gmail.com>
2024-06-16 20:17:05 +0200

    Update Variables.js

--- Variables.js
+++ Variables.js
@@ -1,9 +1,16 @@
 // Variable Declaration
-let name = "Eunice";
-let age = 25;
-let isStudent = true;
+let name = "Eunice"; // Declare a variable 'name'
+let age = 25; // Declare a variable 'age' 
+let isStudent = true; // Declare a variable 'isStudent' and assign it a boolean value indicating whether i am a student or not
 
 // Print all three variables
-console.log(name);
-console.log(age);
-console.log(isStudent);
+console.log(name); // Print the value of 'name' to the console
+console.log(age); // Print the value of 'age' to the console
+console.log(isStudent); // Print the value of 'isStudent' to the console
+
+// If statement
+if (isStudent) {
+    console.log(`${name} is a student.`); // Print a message indicating that 'name' is a student if 'isStudent' is true
+} else {
+    console.log(`${name} is not a student.`); // Print a message indicating that 'name' is not a student if 'isStudent' is false
+}
