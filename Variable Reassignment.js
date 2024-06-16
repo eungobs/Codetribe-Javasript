ae3d02225a725f04717eab5910a9d137432fdfce
eungobs <eungobs@gmail.com>
2024-06-16 20:30:46 +0200

    Create Variable Reassignment.js

---
+++ Variable Reassignment.js
@@ -0,0 +1,12 @@
+// Variable Reassignment
+// Declaring a variable ‘favoriteColour’ and assign your favourite colour to it.
+let favoriteColour = "blue";
+
+// Print the value of favoriteColour.
+console.log("Initial favorite colour:", favoriteColour);
+
+// Reassign favouriteColour to a different colour.
+favoriteColour = "green";
+
+// Print the new value of favouriteColour.
+console.log("Updated favorite colour:", favoriteColour);
